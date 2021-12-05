2e9dbeaa5af314caa1d5cb52c18095a1eb7e1a37
evgeniy <not-found@inbox.ru>
2021-12-05 19:54:34 +0300

    Улучшена валидация

--- js-basic/2/2.3/js/script.js
+++ js-basic/2/2.3/js/script.js
@@ -6,11 +6,11 @@ const myIterable = {
 
 myIterable[Symbol.iterator] = function() {
   if (this.from && this.to) {
-    if (typeof(this.from) !== 'number') {
-      throw new Error('Свойство "from" - не число');
+    if (!Number.isInteger(this.from)) {
+      throw new Error('Свойство "from" - не целое число');
     }
-    if (typeof(this.to) !== 'number') {
-      throw new Error('Свойство "to" - не число');
+    if (!Number.isInteger(this.to)) {
+      throw new Error('Свойство "to" - не целое число');
     }
     if (this.to < this.from) {
       throw new Error('"to" не может быть меньше "from"');
